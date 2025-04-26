0d26ee89a27c58493f9a97c001f6731f9a25bc48
YiTsenHo <yvonne20050822@icloud.com>
2025-04-26 23:14:14 +0800

    Update paths

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,9 +3,9 @@ export const projectsData = [
     id: 2,
     title: "即時路況 App UI 設計",
     imageUrl: [
-      "/images/projects/project2/1.png",
-      "/images/projects/project2/2.png",
-      "/images/projects/project2/3.png",
+      `${import.meta.env.BASE_URL}images/projects/project2/1.png`,
+      `${import.meta.env.BASE_URL}images/projects/project2/2.png`,
+      `${import.meta.env.BASE_URL}images/projects/project2/3.png`,
     ],
     category: "UI 設計"
   },
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/',  // 添加這行，需要對應你的 GitHub repo 名稱
   plugins: [react()],
 })
